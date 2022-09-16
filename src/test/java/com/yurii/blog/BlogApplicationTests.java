d92001edb98a392321cde62d666e733fb1060613
yuriilevkovich <yurii.levkovich@gmail.com>
2022-09-16 14:44:18 +0200

    First commit

---
+++ src/test/java/com/yurii/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yurii.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

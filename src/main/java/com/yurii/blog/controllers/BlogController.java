14bf7fec558f68d4c19ebfa480e23ac5ef6dc919
yuriilevkovich <yurii.levkovich@gmail.com>
2022-09-16 18:22:30 +0200

    CR Posts with mysql

--- src/main/java/com/yurii/blog/controllers/BlogController.java
+++ src/main/java/com/yurii/blog/controllers/BlogController.java
@@ -1,15 +1,36 @@
 package com.yurii.blog.controllers;
 
+import com.yurii.blog.models.Post;
+import com.yurii.blog.repositories.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BlogController {
 
+    @Autowired
+    private PostRepository postRepository;
+
     @GetMapping("/blog")
     public String blog(Model model) {
-        model.addAttribute("title", "Blog Page");
+        Iterable<Post> posts = postRepository.findAll();
+        model.addAttribute("posts", posts);
         return "blog";
+    }
+
+    @GetMapping("/blog/add")
+    public String blogAdd(Model model) {
+        return "blog-add";
+    }
+
+    @PostMapping("/blog/add")
+    public String blogPostAdd(@RequestParam String title, @RequestParam String anons, @RequestParam String full_text, Model model) {
+        Post post = new Post(title, anons, full_text);
+        postRepository.save(post);
+        return "redirect:/blog";
     }
 }
---
+++ src/main/java/com/yurii/blog/repositories/PostRepository.java
@@ -0,0 +1,7 @@
+package com.yurii.blog.repositories;
+
+import com.yurii.blog.models.Post;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PostRepository extends CrudRepository<Post, Long> {
+}

b4c5c9cfd633d48f1760dbd2bff9fffebcc87bc6
yuriilevkovich <yurii.levkovich@gmail.com>
2022-09-28 23:52:46 +0200

    static page about

--- src/main/java/maramax/controllers/MainController.java
+++ src/main/java/maramax/controllers/MainController.java
@@ -11,12 +11,16 @@ public class MainController {
     @GetMapping("/")
     public String home(Model model) {
         model.addAttribute("title", "Main Page");
+        model.addAttribute("style", "/css/index.min.css");
         return "home";
     }
 
     @GetMapping("/about")
     public String about(Model model) {
         model.addAttribute("title", "About Page");
+        //todo to add if empty
+        model.addAttribute("style", "/css/about.min.css");
+        model.addAttribute("script", "/js/about.min.js");
         return "about";
     }
 }

0458ec05c419e2f8dfbed6d8824a52b23a5de0eb
yuriilevkovich <yurii.levkovich@gmail.com>
2022-11-03 15:57:20 +0100

    deploy fix

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,7 +13,7 @@ echo 'Restart server...'
 ssh -i ~/.ssh/id_rsa root@185.65.247.25 << EOF
 
 pgrep java | sudo xargs kill -9
-nohup java -jar maramax-0.0.1-SNAPSHOT.jar > log.txt &
+nohup java -jar /home/maramax/maramax-0.0.1-SNAPSHOT.jar > log.txt &
 
 EOF
 

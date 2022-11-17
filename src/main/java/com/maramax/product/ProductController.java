14099e6cf2760fd240be914a3524e342119143e9
yuriilevkovich <yurii.levkovich@gmail.com>
2022-11-18 00:51:57 +0100

    little dynamic products

--- src/main/java/com/maramax/product/ProductController.java
+++ src/main/java/com/maramax/product/ProductController.java
@@ -10,6 +10,15 @@ public class ProductController {
     @Autowired
     private ProductRepository productRepository;
 
+    @GetMapping(value={"/products", "/products/pos-materials"})
+    public String productsPosMaterials(Model model) {
+        model.addAttribute("products", this.productRepository.findByType("pos-materials"));
+        model.addAttribute("title", "Products Page");
+        model.addAttribute("style", "/css/products.min.css");
+        model.addAttribute("script", "/js/production.min.js");
+        return "products/pos-materials";
+    }
+
     @GetMapping("/products/cashed")
     public String productsCashed(Model model) {
         model.addAttribute("products", this.productRepository.findByType("cashed"));
@@ -18,28 +27,22 @@ public class ProductController {
         model.addAttribute("script", "/js/production.min.js");
         return "products/cashed";
     }
-//
-//    @GetMapping(value={"/products", "/products/pos-materials"})
-//    public String productsPosMaterials(Model model) {
-//        model.addAttribute("title", "Products Page");
-//        model.addAttribute("style", "/css/products.min.css");
-//        model.addAttribute("script", "/js/production.min.js");
-//        return "products/pos-materials";
-//    }
-//
-//    @GetMapping("/products/cartboard")
-//    public String productsCartboard(Model model) {
-//        model.addAttribute("title", "Products Page");
-//        model.addAttribute("style", "/css/products.min.css");
-//        model.addAttribute("script", "/js/production.min.js");
-//        return "products/cartboard";
-//    }
-//
-//    @GetMapping("/products/corrugated-cartboard")
-//    public String productsCorrugatedCartboard(Model model) {
-//        model.addAttribute("title", "Products Page");
-//        model.addAttribute("style", "/css/products.min.css");
-//        model.addAttribute("script", "/js/production.min.js");
-//        return "products/corrugated-cartboard";
-//    }
+
+    @GetMapping("/products/cartboard")
+    public String productsCartboard(Model model) {
+        model.addAttribute("products", this.productRepository.findByType("cartboard"));
+        model.addAttribute("title", "Products Page");
+        model.addAttribute("style", "/css/products.min.css");
+        model.addAttribute("script", "/js/production.min.js");
+        return "products/cartboard";
+    }
+
+    @GetMapping("/products/corrugated")
+    public String productsCorrugatedCartboard(Model model) {
+        model.addAttribute("products", this.productRepository.findByType("corrugated"));
+        model.addAttribute("title", "Products Page");
+        model.addAttribute("style", "/css/products.min.css");
+        model.addAttribute("script", "/js/production.min.js");
+        return "products/corrugated";
+    }
 }

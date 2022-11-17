eea132d4e7833391ce95a11b954996618869da07
yuriilevkovich <yurii.levkovich@gmail.com>
2022-11-18 00:18:28 +0100

    cashed page only work

--- src/main/java/com/maramax/product/ProductController.java
+++ src/main/java/com/maramax/product/ProductController.java
@@ -12,34 +12,34 @@ public class ProductController {
 
     @GetMapping("/products/cashed")
     public String productsCashed(Model model) {
-        model.addAttribute("products", this.productRepository.findAll());
+        model.addAttribute("products", this.productRepository.findByType("cashed"));
         model.addAttribute("title", "Products Page");
         model.addAttribute("style", "/css/products.min.css");
         model.addAttribute("script", "/js/production.min.js");
         return "products/cashed";
     }
-
-    @GetMapping(value={"/products", "/products/pos-materials"})
-    public String productsPosMaterials(Model model) {
-        model.addAttribute("title", "Products Page");
-        model.addAttribute("style", "/css/products.min.css");
-        model.addAttribute("script", "/js/production.min.js");
-        return "products/pos-materials";
-    }
-
-    @GetMapping("/products/cartboard")
-    public String productsCartboard(Model model) {
-        model.addAttribute("title", "Products Page");
-        model.addAttribute("style", "/css/products.min.css");
-        model.addAttribute("script", "/js/production.min.js");
-        return "products/cartboard";
-    }
-
-    @GetMapping("/products/corrugated-cartboard")
-    public String productsCorrugatedCartboard(Model model) {
-        model.addAttribute("title", "Products Page");
-        model.addAttribute("style", "/css/products.min.css");
-        model.addAttribute("script", "/js/production.min.js");
-        return "products/corrugated-cartboard";
-    }
+//
+//    @GetMapping(value={"/products", "/products/pos-materials"})
+//    public String productsPosMaterials(Model model) {
+//        model.addAttribute("title", "Products Page");
+//        model.addAttribute("style", "/css/products.min.css");
+//        model.addAttribute("script", "/js/production.min.js");
+//        return "products/pos-materials";
+//    }
+//
+//    @GetMapping("/products/cartboard")
+//    public String productsCartboard(Model model) {
+//        model.addAttribute("title", "Products Page");
+//        model.addAttribute("style", "/css/products.min.css");
+//        model.addAttribute("script", "/js/production.min.js");
+//        return "products/cartboard";
+//    }
+//
+//    @GetMapping("/products/corrugated-cartboard")
+//    public String productsCorrugatedCartboard(Model model) {
+//        model.addAttribute("title", "Products Page");
+//        model.addAttribute("style", "/css/products.min.css");
+//        model.addAttribute("script", "/js/production.min.js");
+//        return "products/corrugated-cartboard";
+//    }
 }
--- src/main/java/com/maramax/product/ProductRepository.java
+++ src/main/java/com/maramax/product/ProductRepository.java
@@ -4,4 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductRepository extends CrudRepository<Product, Long> {}
+public interface ProductRepository extends CrudRepository<Product, Long> {
+    Iterable<Product> findByType(String type);
+}

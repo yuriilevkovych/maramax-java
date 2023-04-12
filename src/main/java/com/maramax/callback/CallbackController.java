0265b4a902f8ebbb9d45e49e613ee4a0c0e96439
yuriilevkovich <yurii.levkovich@gmail.com>
2023-04-12 17:42:54 +0200

    Captcha

--- src/main/java/com/maramax/callback/CallbackController.java
+++ src/main/java/com/maramax/callback/CallbackController.java
@@ -1,19 +1,33 @@
 package com.maramax.callback;
 
+import com.github.mkopylec.recaptcha.validation.RecaptchaValidator;
+import com.github.mkopylec.recaptcha.validation.ValidationResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Recaptcha: <a href="https://github.com/mkopylec/recaptcha-spring-boot-starter">How to use</a>
+ */
 @Controller
 public class CallbackController {
     @Autowired
     private CallbackService callbackService;
 
+    @Autowired
+    private RecaptchaValidator recaptchaValidator;
+
     @PostMapping("/callback")
-    public String callback(Callback callback) {
-        callbackService.sendCallbackMessage(callback);
+    public String validateCaptcha(HttpServletRequest request, Callback callback) {
+        ValidationResult result = recaptchaValidator.validate(request);
+        if (result.isSuccess()) {
+            callbackService.sendCallbackMessage(callback);
 
-        return "redirect:/";
+            return "redirect:/";
+        } else {
+            return "Callback form error: " + request.toString();
+        }
     }
-
 }

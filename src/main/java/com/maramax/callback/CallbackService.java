7a398b19cca2d5cb453131e56e0eb3f45ad7ef2b
yuriilevkovich <yurii.levkovich@gmail.com>
2023-05-18 23:35:32 +0200

    Test adding simple callback check

--- src/main/java/com/maramax/callback/CallbackService.java
+++ src/main/java/com/maramax/callback/CallbackService.java
@@ -11,11 +11,11 @@ public class CallbackService {
 
     public void sendCallbackMessage(Callback callback) {
         String message = String.format(
-            "Name: %s\n" +
+            "Імя: %s\n" +
             "Email: %s\n" +
-            "Tel: %s\n" +
-            "Count: %s\n" +
-            "Message: %s\n",
+            "Телефон: %s\n" +
+            "Кількість: %s\n" +
+            "Повідомлення: %s\n",
                 callback.getName(),
                 callback.getEmail(),
                 callback.getTel(),
@@ -23,9 +23,9 @@ public class CallbackService {
                 callback.getMessage()
         );
 
-        mailSenderService.sendEmail("yu.destroyer.ok@gmail.com", "Заявка на зворотній звязок", message);
+        mailSenderService.sendEmail("yu.destroyer.ok@gmail.com", "Зворотній звязок", message);
 //        mailSenderService.sendEmail("office@maramax.kiev.ua", "Callback form Maramax", message);
-        mailSenderService.sendEmail("m.kysilov@maramax.kiev.ua", "Заявка на зворотній звязок", message);
-        mailSenderService.sendEmail("a.martynenko@maramax.kiev.ua", "Заявка на зворотній звязок", message);
+//        mailSenderService.sendEmail("m.kysilov@maramax.kiev.ua", "Заявка на зворотній звязок", message);
+//        mailSenderService.sendEmail("a.martynenko@maramax.kiev.ua", "Заявка на зворотній звязок", message);
     }
 }
